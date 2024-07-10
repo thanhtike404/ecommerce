d4fe1fa6d3a47c16bbd3d438a31414a4902bc6ad
thanhtikezaw <thanhtikezaw919@gmail.com>
2024-07-10 17:33:48 +0700

    added secret key on next auth for middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,15 @@
-export { default } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export default async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/dashboard/:path*', '/orders', '/checkout'],
