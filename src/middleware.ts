e0bbb9f16a0c407c96fa17f7d318f18aa16a22c0
thanhtikezaw <thanhtikezaw919@gmail.com>
2024-07-13 00:40:20 +0700

    fixed middleware errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,5 @@
-// middleware.ts
-import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
-  if (!token) {
-    // User is not authenticated
-    return NextResponse.redirect(new URL('/auth/signin', req.url));
-  }
-
-  // Continue to the requested page or API route
-  return NextResponse.next();
-}
-// Config to specify the routes to apply the middleware to
 export const config = {
   matcher: ['/dashboard', '/checkout', '/orders'],
 };
